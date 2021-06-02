f4e748a052f31797afe8c2143f7579ddeea6b02e
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-06-03 01:19:32 +0300

    Create scancode_demo.py

---
+++ scancode_demo.py
@@ -0,0 +1,21 @@
+import evdev, time
+
+device = None
+while device is None:
+    try:
+        device = evdev.InputDevice('/dev/input/event5')
+    except:
+        print('Waiting for keyboard')
+        time.sleep(3)
+
+print('Keyboard connection established')
+device.grab()
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+        key_event = evdev.categorize(event)
+        if key_event.keystate == 1:
+            print(key_event.scancode, key_event.keycode)
+
+
+

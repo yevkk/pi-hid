77b0cdc374d76d74f34c19c50e0dd948b3e29a1c
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-06-13 14:34:43 +0300

    Update scancode_demo.py

--- scancode_demo.py
+++ scancode_demo.py
@@ -15,7 +15,7 @@ for event in device.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
         key_event = evdev.categorize(event)
         if key_event.keystate == 1:
-            print(key_event.scancode, key_event.keycode)
+            print(key_event.scancode, hex(key_event.scancode), key_event.keycode)
 
 
 
